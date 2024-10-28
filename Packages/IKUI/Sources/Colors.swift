7b18a599be3a277567d5529d505743a8d5f14171
Igor Klyuzhev <klyuzhevigor@gmail.com>
2024-10-28 21:31:42 +0300

    fix: Added theme dependent colors

--- Packages/IKUI/Sources/Colors.swift
+++ Packages/IKUI/Sources/Colors.swift
@@ -1,18 +1,15 @@
-//
-//  File.swift
-//  IKUI
-//
-//  Created by Igor Kluzhev on 28.10.2024.
-//
+// Created by Igor Klyuzhev in 2024
 
 import UIKit
 
 public enum Colors {
   public static let accent = UIColor(hex: "#A8DB10FF")
-  public static let background = UIColor(hex: "#FFFFFFFF")
-  public static let foreground = UIColor(hex: "#000000FF")
+  public static let background = UIColor(light: Palette.white, dark: Palette.black)
+  public static let foreground = UIColor(light: Palette.black, dark: Palette.white)
 
   enum Palette {
+    public static let white = UIColor(hex: "#FFFFFFFF")
+    public static let black = UIColor(hex: "#000000FF")
     public static let blue = UIColor(hex: "#1976D2FF")
     public static let red = UIColor(hex: "#FF3D00FF")
     public static let gray = UIColor(hex: "#8B8B8BFF")
--- Packages/IKUI/Sources/UIColor+Extensions.swift
+++ Packages/IKUI/Sources/UIColor+Extensions.swift
@@ -3,29 +3,42 @@
 import UIKit
 
 extension UIColor {
-  public convenience init?(hex: String) {
+  public convenience init(hex: String) {
     let r, g, b, a: CGFloat
 
-    if hex.hasPrefix("#") {
-      let start = hex.index(hex.startIndex, offsetBy: 1)
-      let hexColor = String(hex[start...])
+    let start = hex.index(hex.startIndex, offsetBy: 1)
+    let hexColor = String(hex[start...])
 
-      if hexColor.count == 8 {
-        let scanner = Scanner(string: hexColor)
-        var hexNumber: UInt64 = 0
+    let scanner = Scanner(string: hexColor)
+    var hexNumber: UInt64 = 0
 
-        if scanner.scanHexInt64(&hexNumber) {
-          r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-          g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-          b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-          a = CGFloat(hexNumber & 0x000000ff) / 255
+    if scanner.scanHexInt64(&hexNumber) {
+      r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+      g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+      b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+      a = CGFloat(hexNumber & 0x000000ff) / 255
 
-          self.init(red: r, green: g, blue: b, alpha: a)
-          return
-        }
+      self.init(red: r, green: g, blue: b, alpha: a)
+      return
+    } else {
+      assertionFailure("Not correct hex string for color initializer - \(hex)")
+      self.init(red: .zero, green: .zero, blue: .zero, alpha: .zero)
+    }
+  }
+
+  public convenience init(
+    light lightColor: @escaping @autoclosure () -> UIColor,
+    dark darkColor: @escaping @autoclosure () -> UIColor
+  ) {
+    self.init { trait in
+      switch trait.userInterfaceStyle {
+      case .light, .unspecified:
+        lightColor()
+      case .dark:
+        darkColor()
+      @unknown default:
+        lightColor()
       }
     }
-    
-    return nil
   }
 }
